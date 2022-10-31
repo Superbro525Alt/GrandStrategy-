89dd7c57fe55ff56c984f98bbe1765daa69098be
JahegaDev <jacob.heath2@student.education.wa.edu.au>
2022-10-31 13:20:31 +0800

    Quit buttons work

--- html/js/index.js
+++ html/js/index.js
@@ -1,9 +1,17 @@
 mainQuit = document.getElementById("mainQuit");
 quitMenu = document.getElementById("quitMenu");
+quitYes = document.getElementById("quitYes");
+quitNo = document.getElementById("quitNo");
 mainQuit.addEventListener("click", function() {
     if (quitMenu.style.display == "none") {
         quitMenu.style.display = "block";
     } else {
         quitMenu.style.display = "none";
     }
+});
+quitYes.addEventListener("click", function() {
+    window.close();
+});
+quitNo.addEventListener("click", function() {
+    quitMenu.style.display = "none";
 });
